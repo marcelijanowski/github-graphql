e4e1265b6ea80c9ead9ed106cf906d9a5367c6be
MJ <marceli@mjdesign.pl>
2020-06-10 09:16:32 +0200

    Add small changes

--- src/component/organisms/User/Search/UserSearch.test.jsx
+++ src/component/organisms/User/Search/UserSearch.test.jsx
@@ -23,9 +23,4 @@ it('should render loading state initially',  () => {
 
   expect(container.getElementsByTagName('h3')[0].innerHTML).toEqual('Search for user information');
 });
-it('should render error', () => {
 
-});
-test('it should render user search with results', () => {
-
-});
